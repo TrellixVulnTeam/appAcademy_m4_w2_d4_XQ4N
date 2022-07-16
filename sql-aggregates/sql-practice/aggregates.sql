4e3203f89eacab18bdb133f27d57379d962846a0
aoavzm <aoavzm@gmail.com>
2022-07-16 10:19:32 -0700

    Part2 Done

--- sql-aggregates/sql-practice/aggregates.sql
+++ sql-aggregates/sql-practice/aggregates.sql
@@ -5,3 +5,18 @@ SELECT min(birth_year) as youngest_cat,
 max(birth_year) as oldest_cat
 FROM cats
 ORDER BY birth_year;
+
+SELECT cats.name, (
+    SELECT COUNT(toys.name) FROM toys
+    WHERE cats.id =  toys.cat_id
+) AS number_of_toys FROM cats
+JOIN toys ON toys.cat_id = cats.id
+GROUP BY cats.name;
+
+SELECT cats.name, (
+    SELECT COUNT(toys.name) FROM toys
+    WHERE cats.id =  toys.cat_id
+) AS number_of_toys FROM cats
+JOIN toys ON toys.cat_id = cats.id
+GROUP BY cats.name
+HAVING number_of_toys >= 2;
